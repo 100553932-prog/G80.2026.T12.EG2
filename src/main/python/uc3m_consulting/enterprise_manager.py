a398f60558cd5953b646a60c24c38c939abc745f
Miguel Moreno <miguelmoreno@MacBook-Air-de-Miguel.local>
2026-03-25 14:10:45 +0100

    Add empty register_project method so initial F1 test fails

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,12 +1,13 @@
 """Module """
 
-class EnterpriseManager:
-    """Class for providing the methods for managing the orders"""
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def validate_cif(cif: str):
-        """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
-        OR FALSE IN OTHER CASE"""
-        return True
+@classmethod
+def register_project(
+    cls,
+    company_cif: str,
+    project_acronym: str,
+    project_description: str,
+    department: str,
+    date: str,
+    budget: float,
+) -> str:
+    pass
