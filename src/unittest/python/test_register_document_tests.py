f4f97d866f5b6cf9d7ecc10a5a261ab7b03f2234
Miguel Moreno <miguelmoreno@MacBook-Air-de-Miguel.local>
2026-03-25 17:27:34 +0100

    Add second F2 test for missing input file

--- src/unittest/python/test_register_document_tests.py
+++ src/unittest/python/test_register_document_tests.py
@@ -1,5 +1,7 @@
 import unittest
+
 from uc3m_consulting import EnterpriseManager
+from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
 
 
 class TestRegisterDocument(unittest.TestCase):
@@ -9,3 +11,7 @@ class TestRegisterDocument(unittest.TestCase):
             "src/unittest/resources/valid_doc.json"
         )
         self.assertRegex(signature, r"^[0-9a-f]{64}$")
+
+    def test_input_file_not_found_raises(self):
+        with self.assertRaises(EnterpriseManagementException):
+            EnterpriseManager.register_document("src/unittest/resources/no_file.json")
